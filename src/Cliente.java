f5ad2c6164822eab044dddfe6f0789a6f0095561
Anth0nYM <68914576+Anth0nYM@users.noreply.github.com>
2021-11-27 08:46:37 -0300

    first commit

---
+++ src/Cliente.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+//import java.util.Scanner;
+public class Cliente {
+    String nome;
+    String CPF;
+    Date dataDeNascimento;
+    String endereço;
+    Conta conta;
+    }
---
+++ src/Conta.java
@@ -0,0 +1,34 @@
+public class Conta{
+private int agencia;
+private int numero;
+private double saldo;
+private Cliente cliente;
+
+public Conta(int agencia, int numero, Cliente cliente) {
+ this.agencia = agencia;
+ this.numero = numero;
+ this.cliente = cliente;
+}
+public void deposita(double valor){
+    this.saldo += valor;
+    System.out.println("Valor depositado, saldo atual: " + saldo);
+}
+public void saca(double valor){
+    if(valor <= saldo){
+        saldo -= valor;
+        System.out.println("Saque realizado, saldo atual: " + saldo);
+    } else{
+        System.out.println("Você não possuí saldo suficiente para sacar esse valor");
+    }
+}
+public int getAgencia() {
+    return agencia;
+}
+public int getNumero() {
+    return numero;
+}
+public Cliente getCliente() {
+    return cliente;
+}
+}
+
