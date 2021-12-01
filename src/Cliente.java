e3e7e4f8a2cdf4563fae0ff76e668859d4aac823
Anth0nYM <68914576+Anth0nYM@users.noreply.github.com>
2021-12-01 10:09:38 -0300

    Adicionando  um construtor, métodos e encapsulamento na classe Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,9 +1,48 @@
 import java.util.Date;
 //import java.util.Scanner;
 public class Cliente {
-    String nome;
-    String CPF;
-    Date dataDeNascimento;
-    String endereço;
-    Conta conta;
+    private String nome;
+    private String CPF;
+    private Date dataDeNascimento;
+    private String endereco;
+    private Conta contas;
+
+    public Cliente(String nome, String CPF, Date dataDeNascimento, String endereco){
+        this.nome = nome;
+        this.CPF = CPF;
+        this.dataDeNascimento = dataDeNascimento;
+        this.endereco = endereco; 
+        }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public Date getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(Date dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Conta getContas() {
+        return contas;
+    }
     }
