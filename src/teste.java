f8d3d65404888a0725dc7a30c9d485683a31c849
Anth0nYM <68914576+Anth0nYM@users.noreply.github.com>
2021-12-14 10:39:45 -0300

    Ajustando a Classe Notificacao e criando um metodo de transferir

--- src/teste.java
+++ src/teste.java
@@ -6,7 +6,5 @@ public class teste{
    Cliente cliente1 = new Cliente ("Anthony","01", datanasc, new Endereco("rua 1",1,"Naturais","cidade dos numeros","IN"));
    Conta conta1 = new Conta(1, 1, cliente1);
    Conta conta2 = new Conta(1,2,cliente1);
-  System.out.println(conta1.getCliente().getNome());
-  System.out.println(conta2.getCliente().getNome());
   }
 }
