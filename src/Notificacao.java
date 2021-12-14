07eee249e575ea99084d78e7e86d3dbad6c29f21
Anth0nYM <68914576+Anth0nYM@users.noreply.github.com>
2021-12-14 10:11:11 -0300

    criando classe Notificacao

---
+++ src/Notificacao.java
@@ -0,0 +1,7 @@
+public class Notificacao {
+    public void sendEmail(String acao, double valor){
+        System.out.println("E-mail enviado");
+        System.out.println("Operação realizada com sucesso:  " + acao);
+        System.out.println("Valor: " + valor);
+    }
+}
