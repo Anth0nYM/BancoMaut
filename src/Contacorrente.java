4dba99ac641bd4e85f9bfb040cedb55d6c2fffba
Anth0nYM <68914576+Anth0nYM@users.noreply.github.com>
2021-12-17 16:15:37 -0300

    ADD classe corrente e classe conta

---
+++ src/Contacorrente.java
@@ -0,0 +1,8 @@
+public class Contacorrente extends Conta{
+
+    public Contacorrente(int agencia, int numero, Cliente cliente) {
+        super(agencia, numero, cliente);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
--- src/teste.java
+++ src/teste.java
@@ -2,16 +2,9 @@ import java.time.LocalDate;
 
 public class teste{
  public static void main(String[]args){
-   LocalDate datanasc = LocalDate.of(2000,5,20);
-   Cliente cliente1 = new Cliente ("Anthony","01", datanasc, new Endereco("rua 1",1,"Naturais","cidade dos numeros","IN"));
-   Conta conta1 = new Conta(1, 1, cliente1);
-   Conta conta2 = new Conta(1,2,cliente1);
-   
-   conta1.deposita(500);
-   conta1.tranfere(250, conta2);
-   conta2.saca(100);
-  
-  System.out.println(conta1.getSaldo());
-  System.out.println(conta2.getSaldo());
-  }
+   Cliente cliente1 = new Cliente("Anthony", "01", LocalDate.of(2000,10,3), 
+   new Endereco("Rua Maria Borges", 10, "Samambaia", "Picos", "PI"));
+   Contacorrente cc = new Contacorrente(1, 1, cliente1);
+   cc.deposita(100);
+ }
 }
