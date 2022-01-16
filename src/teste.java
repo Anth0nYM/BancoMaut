c0ed5e9ee10ec17b90e54669dc0aba9721d4f423
Anth0nYM <marquesanthony62@gmail.com>
2022-01-16 07:45:02 -0300

    last commit

--- src/teste.java
+++ src/teste.java
@@ -1,10 +1,16 @@
 import java.time.LocalDate;
-
-public class teste{
- public static void main(String[]args){
-   Cliente cliente1 = new Cliente("Anthony", "01", LocalDate.of(2000,10,3), 
-   new Endereco("Rua Maria Borges", 10, "Samambaia", "Picos", "PI"));
-   Conta cc = new Contacorrente(1, 1, cliente1);
-   Conta cp = new Contapoupanca(1, 2, cliente1);
+public class teste {
+  public static void main(String[] args) {
+    Cliente cliente1 = new Cliente("Anthony", "01", LocalDate.of(2000, 10, 3),
+        new Endereco("Rua Maria Borges", 10, "Samambaia", "Picos", "PI"));
+    Conta cc = new Contacorrente(1, 1, cliente1);
+    Conta cp = new Contapoupanca(1, 2, cliente1);
+    cp.deposita(100);
+    cc.deposita(100);
+    cc.tranfere(50, cp);
+    cc.tranfere(10, cp);
+    cc.tranfere(40, cp);
+    System.out.println(cc.getSaldo());
+    System.out.println(cp.getSaldo());
   }
-}  
+}
